11b506eaa9293f88e4d3e7fa59c0f3566e90ea5d
hernand.azevedo@gmail.com <hernand.azevedo@gmail.com@a17f460f-8b45-319f-670e-d3fd423f3bcb>
2013-02-17 21:33:01 +0000

    Estrutura dos serviços

---
+++ trunk/emprestimo-infnet-core/src/dominio/dao/EmpregadoDAO.java
@@ -0,0 +1,56 @@
+package dominio.dao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import dominio.dto.EmpregadoDTO;
+
+public class EmpregadoDAO  implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3450244970210084606L;
+	
+	private static EmpregadoDAO instance = null;
+	
+	private List<EmpregadoDTO> empregados = mockEmpregados();
+	
+	
+	/**
+	 * Mock da lista inicial de emprestimos contratados 
+	 * @return
+	 */
+	private List<EmpregadoDTO> mockEmpregados() {
+		
+		if(empregados == null){
+			empregados = new ArrayList<EmpregadoDTO>();
+			EmpregadoDTO e1 = new EmpregadoDTO();
+			//TODO preencher os dados do empregado
+			empregados.add(e1);
+			
+		}
+		return empregados;
+	}
+	
+	
+	
+	public static EmpregadoDAO getInstance(){
+		if(instance == null){
+			instance = new EmpregadoDAO();
+		}
+		return instance;
+	}
+	
+	
+	public List<EmpregadoDTO> getEmpregados() {
+		return empregados;
+	}
+	
+	
+	public void salvarContratoEmprestimo(EmpregadoDTO e){
+		getEmpregados().add(e);
+	}
+	
+}
