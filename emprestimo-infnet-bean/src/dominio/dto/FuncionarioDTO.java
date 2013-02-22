79d5255fb391308abfedc40e7d8f0e6202caf0de
williamnwz@gmail.com <williamnwz@gmail.com@a17f460f-8b45-319f-670e-d3fd423f3bcb>
2013-02-22 01:44:16 +0000

    Atributos para os DTOs

--- emprestimo-infnet-bean/src/dominio/dto/FuncionarioDTO.java
+++ emprestimo-infnet-bean/src/dominio/dto/FuncionarioDTO.java
@@ -3,10 +3,45 @@ package dominio.dto;
 import java.io.Serializable;
 
 public class FuncionarioDTO  implements Serializable{
+
 	
-	/**
-	 * 
-	 */
+	private int id_funcionario;
+	private String nome;
+	private PerfilFuncionarioDTO perfil;
+	
+	
+	
+	
+	public PerfilFuncionarioDTO getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(PerfilFuncionarioDTO perfil) {
+		this.perfil = perfil;
+	}
+	public int getId_funcionario() {
+		return id_funcionario;
+	}
+
+
+
+	public void setId_funcionario(int id_funcionario) {
+		this.id_funcionario = id_funcionario;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
 	private static final long serialVersionUID = 1L;
 
 }
