06734847ccc3f0aabcac8a36eeb932cbbd8b0415
hernand.azevedo@gmail.com <hernand.azevedo@gmail.com@a17f460f-8b45-319f-670e-d3fd423f3bcb>
2013-02-23 19:34:05 +0000

    Estrutura dos serviços

--- emprestimo-infnet-bean/src/dominio/dto/EmpresaConvenenteDTO.java
+++ emprestimo-infnet-bean/src/dominio/dto/EmpresaConvenenteDTO.java
@@ -10,7 +10,23 @@ public class EmpresaConvenenteDTO  implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	//Um para muitos
 	private List<FuncionarioDTO> funcionarios;
+	//Muitos para muitos... ver InstituicaoFinanceira
+	private List<ConvenioDTO> convenios;
+	public List<FuncionarioDTO> getFuncionarios() {
+		return funcionarios;
+	}
+	public void setFuncionarios(List<FuncionarioDTO> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+	public List<ConvenioDTO> getConvenios() {
+		return convenios;
+	}
+	public void setConvenios(List<ConvenioDTO> convenios) {
+		this.convenios = convenios;
+	}
+	
 	
 
 }
--- emprestimo-infnet-bean/src/dominio/dto/InstituicaoFinanceiraDTO.java
+++ emprestimo-infnet-bean/src/dominio/dto/InstituicaoFinanceiraDTO.java
@@ -11,7 +11,22 @@ public class InstituicaoFinanceiraDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private List<PlanoEmprestimoDTO> planosEmprestimos;
+	//Muitos para muitos... ver EmpresaConvenente
+	private List<ConvenioDTO> convenios;
+	public List<PlanoEmprestimoDTO> getPlanosEmprestimos() {
+		return planosEmprestimos;
+	}
+	public void setPlanosEmprestimos(List<PlanoEmprestimoDTO> planosEmprestimos) {
+		this.planosEmprestimos = planosEmprestimos;
+	}
+	public List<ConvenioDTO> getConvenios() {
+		return convenios;
+	}
+	public void setConvenios(List<ConvenioDTO> convenios) {
+		this.convenios = convenios;
+	}
 	
 	
-
+	
+	
 }
